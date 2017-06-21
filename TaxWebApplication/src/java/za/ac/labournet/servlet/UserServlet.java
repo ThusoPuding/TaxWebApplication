bc37b341509624c89f3b66cd374f686145c5d444
PUDING <thusopuding@gmail.com>
2017-06-21 09:22:10 +0200

    Servlet updated (UserServlet.java).

--- TaxWebApplication/src/java/za/ac/labournet/servlet/UserServlet.java
+++ TaxWebApplication/src/java/za/ac/labournet/servlet/UserServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author PUDING
  */
-public class UserServlet extends HttpServlet {
+public class UserServlet extends   HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
